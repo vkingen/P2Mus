f3ef13bd1b60694e27648b77fa4b5346d6b003e6
vkingen <lassevikingkristensen@gmail.com>
2022-04-22 15:09:18 +0200

    4

---
+++ P2NatMus/Assets/SceneManagerTemp.cs
@@ -0,0 +1,25 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class SceneManagerTemp : MonoBehaviour
+{
+    SceneManagerTemp instance;
+
+    private void Start()
+    {
+        if (instance != null) // && instance != this)
+            Destroy(this.gameObject);
+        else
+        {
+            instance = this;
+            DontDestroyOnLoad(this.gameObject);
+        }
+    }
+
+    public void ChangeScene()
+    {
+        SceneManager.LoadScene(SceneManager.GetActiveScene().buildIndex + 1);
+    }
+}
