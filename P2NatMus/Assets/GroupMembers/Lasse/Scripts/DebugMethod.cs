545234d66256e10ee20199e1a4d11d779d78a815
vkingen <lassevikingkristensen@gmail.com>
2022-04-27 14:24:34 +0200

    2

---
+++ P2NatMus/Assets/GroupMembers/Lasse/Scripts/DebugMethod.cs
@@ -0,0 +1,16 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class DebugMethod : MonoBehaviour
+{
+    public string debug01, debug02;
+    public void DebugMethod01()
+    {
+        Debug.Log(debug01);
+    }
+    public void DebugMethod02()
+    {
+        Debug.Log(debug02);
+    }
+}
