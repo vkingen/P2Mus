a856ab13f8a837a75d20ad99067dc2d672dd18e5
Marius-Arnbjerg <marius.arnbjerg@gmail.com>
2022-04-27 10:08:40 +0200

    Marius Prøver at lave noget der virker

---
+++ P2NatMus/Assets/GroupMembers/Marius/Scripts/OnTouchInfo.cs
@@ -0,0 +1,39 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class OnTouchInfo : MonoBehaviour
+{
+    public GameObject infoText;
+    private float _touchCounter = 0;
+
+    public void Awake()
+    {
+        infoText.SetActive(false);
+    }
+
+    private void Start()
+    {
+        GetComponent<Canvas>();
+    }
+
+    private void Update()
+    {
+        switch (_touchCounter)
+        {
+            case 1:
+                infoText.SetActive(true);
+                break;
+
+            case 2:
+                infoText.SetActive(false);
+                _touchCounter = 0;
+                break;
+        }
+    }
+
+    public void WhenButtonPressed()
+    {
+  
+    }
+}
