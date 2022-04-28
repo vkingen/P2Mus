fc92ba89a14b991f57f17e50124632dd0fb3ade2
Marius-Arnbjerg <marius.arnbjerg@gmail.com>
2022-04-28 12:39:06 +0200

    Marius' ændringer

--- P2NatMus/Assets/GroupMembers/Marius/Scripts/OnTouchInfo.cs
+++ P2NatMus/Assets/GroupMembers/Marius/Scripts/OnTouchInfo.cs
@@ -5,31 +5,66 @@ using UnityEngine;
 public class OnTouchInfo : MonoBehaviour
 {
     public GameObject infoText;
-    private float _touchCounter = 0;
+    
+    private float _touchCounter = 0; //This variable keeps track of whether the infoText is active(1) or not(0).
+    private float _touchTime; //Keeps track of time since start of program.
 
-    public void Awake()
+    public float touchThresh = 0.2f; //Max amount of time a touch can last for infoText to displayed. 
+
+    private void Awake()
     {
         infoText.SetActive(false);
     }
 
-
     private void Update()
     {
-        switch (_touchCounter)
+        if(_touchCounter == 0)
         {
-            case 1:
-                infoText.SetActive(true);
-                break;
-
-            case 2:
-                infoText.SetActive(false);
-                _touchCounter = 0;
-                break;
+            SetInfoTextActive();
+        }
+        else if (_touchCounter == 1)
+        {
+            InfoTextInactive();
         }
     }
 
-    public void WhenButtonPressed()
+    private void SetInfoTextActive()
     {
-        
+        if (Input.touchCount == 1) //If one finger touches the screen
+        {
+            if (Input.GetTouch(0).phase == TouchPhase.Began) //Start of touch
+            {
+                _touchTime = Time.time; //Assigns _touchTime the timed value since start of program.
+            }
+
+            if (Input.GetTouch(0).phase == TouchPhase.Ended || Input.GetTouch(0).phase == TouchPhase.Canceled) //End of touch
+            {
+                if (Time.time - _touchTime <= touchThresh) //If the time from start of touch to end of touch is less than touchThresh
+                {
+                    infoText.SetActive(true);
+                    _touchCounter++; //Adds 1 to _touchCounter to indicate that infoText is active
+                }
+            }
+        }
+    }
+
+    private void InfoTextInactive()
+    {
+        if (Input.touchCount == 1)
+        {
+            if (Input.GetTouch(0).phase == TouchPhase.Began)
+            {
+                _touchTime = Time.time;
+            }
+
+            if (Input.GetTouch(0).phase == TouchPhase.Ended || Input.GetTouch(0).phase == TouchPhase.Canceled)
+            {
+                if (Time.time - _touchTime <= touchThresh)
+                {
+                    infoText.SetActive(false);
+                    _touchCounter = 0; //Resets _touchCounter so the process can be repeated.
+                }
+            }
+        }
     }
 }
