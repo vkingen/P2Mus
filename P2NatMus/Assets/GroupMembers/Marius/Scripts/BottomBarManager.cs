c151937b19b8e2f888397f12db4a14bcc7780c95
Marius-Arnbjerg <marius.arnbjerg@gmail.com>
2022-05-01 21:08:55 +0200

    PageSwiper

---
+++ P2NatMus/Assets/GroupMembers/Marius/Scripts/BottomBarManager.cs
@@ -0,0 +1,23 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class BottomBarManager : MonoBehaviour
+{
+    Image image;
+
+    float transparency;
+
+
+    // Start is called before the first frame update
+    public void SetTransparency()
+    {
+        if (image != null)
+        {
+            UnityEngine.Color __alpha = image.color;
+            __alpha.a = transparency;
+            image.color = __alpha;
+        }
+    }
+}
--- P2NatMus/Assets/GroupMembers/Marius/Scripts/PageSwiper.cs
+++ P2NatMus/Assets/GroupMembers/Marius/Scripts/PageSwiper.cs
@@ -2,52 +2,68 @@
 using System.Collections.Generic;
 using UnityEngine;
 using UnityEngine.EventSystems;
+using UnityEngine.UI;
 
 //Source: https://pressstart.vip/tutorials/2019/05/15/95/swiping-pages-in-unity.html
 
-public class PageSwiper : MonoBehaviour, IDragHandler, IEndDragHandler
-{
-    private Vector3 panelLocation;
-    public float percentThreshold = 0.2f;
-    public float easing = 0.5f;
-    public int totalPages = 1;
-    private int currentPage = 1;
+//This script should be placed on 'Panel'
 
-    // Start is called before the first frame update
+public class PageSwiper : MonoBehaviour, IDragHandler, IEndDragHandler //These two classes uses built in functions to detect when a touch is dragged and when that drag has ended.
+{
+    public Vector3 panelLocationVector; //Stores location of the panel
+
+    public float percentThreshold = 0.2f; //Threshold that decides how large a swipe has to be to change screen.
+    public float easing = 0.5f; 
+
+    public int totalPages = 1;
+    private int _currentPage = 1; //Keeps track of the page that is currently visible
+
+
+
     void Start()
     {
-        panelLocation = transform.position;
+        panelLocationVector = transform.position; //Sets location of the panel to its current location
     }
+
+
     public void OnDrag(PointerEventData data)
     {
-        float difference = data.pressPosition.x - data.position.x;
-        transform.position = panelLocation - new Vector3(difference, 0, 0);
+        float difference = data.pressPosition.x - data.position.x; //'difference' gets the x position values from start of drag to end of drag to see how far we've dragged. 
+        transform.position = panelLocationVector - new Vector3(difference, 0, 0); //Subtracts the difference of how far we've dragged from the panels idle state.
     }
+
+
     public void OnEndDrag(PointerEventData data)
     {
-        float percentage = (data.pressPosition.x - data.position.x) / Screen.width;
-        if (Mathf.Abs(percentage) >= percentThreshold)
+        float percentage = (data.pressPosition.x - data.position.x) / Screen.width; //percentage variable that gets x values of the swipe to see direction of the swipe,
+                                                                                    //and divides it with screen width to see how much of the screen was swiped across. 
+
+        if (Mathf.Abs(percentage) >= percentThreshold) //If the 'percentage' variable is larger than percentThreshhold 
         {
-            Vector3 newLocation = panelLocation;
-            if (percentage > 0 && currentPage < totalPages)
+            Vector3 newLocation = panelLocationVector; //Create newLocation Vector3 variable that stores PanelLocation 
+
+            if (percentage > 0 && _currentPage < totalPages) //If percentage is positive (x values of swipe above 0) and the _currentpage nr. is smaller than the amount of totalPages
             {
-                currentPage++;
-                newLocation += new Vector3(-Screen.width, 0, 0);
+                _currentPage++; //Add one to _currentPage
+                newLocation += new Vector3(-Screen.width, 0, 0); //And set new panelLocation = Change screen
             }
-            else if (percentage < 0 && currentPage > 1)
+            else if (percentage < 0 && _currentPage > 1) //If percentage variable is negative (x values of swipe below 0) and current page above 1
             {
-                currentPage--;
-                newLocation += new Vector3(Screen.width, 0, 0);
+                _currentPage--; //Minus one to _currentPage
+                newLocation += new Vector3(Screen.width, 0, 0); //And set new panelLocation = Change screen
             }
-            StartCoroutine(SmoothMove(transform.position, newLocation, easing));
-            panelLocation = newLocation;
+
+            StartCoroutine(SmoothMove(transform.position, newLocation, easing)); //Smoothens the transition between screens
+
+            panelLocationVector = newLocation; //Assigns the panelLocation the new screen location
         }
         else
         {
-            StartCoroutine(SmoothMove(transform.position, panelLocation, easing));
+            StartCoroutine(SmoothMove(transform.position, panelLocationVector, easing)); //If 'percentage' is smaller than percentThreshold, smoothen transition back to current panelLocation. 
         }
     }
-    IEnumerator SmoothMove(Vector3 startpos, Vector3 endpos, float seconds)
+
+    IEnumerator SmoothMove(Vector3 startpos, Vector3 endpos, float seconds) //IEnumerator that smoothens swipes
     {
         float t = 0f;
         while (t <= 1.0)
@@ -56,5 +72,101 @@ public class PageSwiper : MonoBehaviour, IDragHandler, IEndDragHandler
             transform.position = Vector3.Lerp(startpos, endpos, Mathf.SmoothStep(0f, 1f, t));
             yield return null;
         }
+    }
+
+    //THE METHODS BELOW HAS TO DO WITH NAVIGATION USING THE BOTTOMBAR
+
+    public Image homeButton, cameraButton, inventoryButton; //The images connected to buttons in bottombar should be assigned accordingly
+
+    public GameObject PanelLocationObject; //The 'Panel' should be assigned to this Gameobject.
+                                           //This gameobject is neccesarry for the panels location to directly, visually, change when using the bottombar. 
+
+
+    public void OnClickHomeBottom() //Should be placed on Home button in bottombar
+    {
+        Vector3 homeScreen = new Vector3(720, 1480, 0); //New vector3 based on the homeScreens location
+        PanelLocationObject.transform.position = homeScreen; //new Vector3(720, 1480, 0);
+        panelLocationVector = homeScreen; //new Vector3(720, 1480, 0);
+
+        //HomeScreen is equal to _currentPage 1,
+        if (_currentPage == 2) //so if the _currentPage is equal to 2(CameraScreen) 
+        {
+            _currentPage--; 
+        }
+        else if(_currentPage == 3) //or 3(InventoryScreen),
+        {
+            _currentPage = 3-2; //then the _currentPage variable should change based on direction of swipe.
+        }
+    }
+
+    public void OnClickCameraBottom() //Should be placed on Camera button in bottombar
+    {
+        Vector3 cameraScreen = new Vector3(-720, 1480, 0); //New vector3 based on the cameraScreens location
+        PanelLocationObject.transform.position = cameraScreen;//new Vector3(-720, 1480, 0);
+        panelLocationVector = cameraScreen; //new Vector3(-720, 1480, 0);
+
+        if (_currentPage == 1)
+        {
+            _currentPage++;
+        }
+        else if (_currentPage == 3)
+        {
+            _currentPage--;
+        }
+    }
+
+    public void OnClickInventoryBottom() //Should be placed on Inventory button in bottombar
+    {
+        Vector3 inventoryScreen = new Vector3(-2160, 1480, 0); //New vector3 based on the inventoryScreens location
+        PanelLocationObject.transform.position = inventoryScreen;//new Vector3(-2160, 1480, 0);
+        panelLocationVector = inventoryScreen;//new Vector3(-2160, 1480, 0);
+
+        if (_currentPage == 1)
+        {            
+            _currentPage = 1+2;
+        }
+        else if (_currentPage == 2)
+        {
+            _currentPage++;
+        }
+    }
+
+    public void SetTransparencyCameraButton(float transparency)
+    { 
+        Color color = cameraButton.color;
+        color.a = transparency;
+        cameraButton.color = color;
+    }
+
+    public void SetTransparencyHomeButton(float transparency)
+    { 
+        Color color = homeButton.color;
+        color.a = transparency;
+        homeButton.color = color;
+    }
+
+    public void SetTransparencyInventoryButton(float transparency)
+    { 
+        Color color = inventoryButton.color;
+        color.a = transparency;
+        inventoryButton.color = color;
+    }
+
+    private void Update()
+    {
+        if (panelLocationVector == new Vector3(720, 1480, 0)) //Transparency of bottom home button
+            SetTransparencyHomeButton(1f);
+        else
+            SetTransparencyHomeButton(0.5f);
+
+        if (panelLocationVector == new Vector3(-720, 1480, 0)) //Transparency of bottom camera button
+            SetTransparencyCameraButton(1f);
+        else
+            SetTransparencyCameraButton(0.5f);
+
+        if (panelLocationVector == new Vector3(-2160, 1480, 0)) //Transparency of bottom inventory button
+            SetTransparencyInventoryButton(1f);
+        else
+            SetTransparencyInventoryButton(0.5f);
     }
 }
