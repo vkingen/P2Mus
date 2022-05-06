32173d99e92e3624b5731db1e1b6579d2947966d
Marius-Arnbjerg <marius.arnbjerg@gmail.com>
2022-05-06 09:36:21 +0200

    Objectinspector

--- P2NatMus/Assets/GroupMembers/Marius/Scripts/ObjectInspector.cs
+++ P2NatMus/Assets/GroupMembers/Marius/Scripts/ObjectInspector.cs
@@ -7,20 +7,19 @@ using UnityEngine;
 //This script should be placed on the Main Camera.
 public class ObjectInspector : MonoBehaviour
 {
-    public GameObject infoText;
     public Transform target; //Target for orbit and zoom
 
     public float distance = 5.0f; //Distance from target if no target is assigned
     public float maxDistance = 20; //Max zoom out distance
     public float minDistance = 1f; //Max zoom in distance
-    public float xSpeed = 40f; //x rotation speed
-    public float ySpeed = 40f; //y rotation speed
+    //public float xSpeed = 40f; //x rotation speed
+    //public float ySpeed = 40f; //y rotation speed
     public int yMinLimit = -90; //y(vertical) rotation limitation
     public int yMaxLimit = 90; //y(vertical) rotation limitation
     public float zoomRate = 40f; //zoom speed
     public float zoomDampening = 5.0f; //Drag when zooming or rotating
-
-    //public float panSpeed = 0.3f; Should maybe replace x and y speed in case of these values always being the same 
+    public float fineTuning = 0.0025f;
+    public float panSpeed = 40f; //Should maybe replace x and y speed in case of these values always being the same 
 
     private float xDeg = 0.0f;
     private float yDeg = 0.0f;
@@ -31,39 +30,6 @@ public class ObjectInspector : MonoBehaviour
     private Quaternion rotation;
     private Vector3 position;
 
-    void Start() 
-    { 
-        Init(); 
-    }
-
-    void OnEnable() 
-    { 
-        Init(); 
-    }
-
-    public void Init()
-    {
-        //If there is no target, create a temporary target at 'distance' from the cameras current viewpoint
-        if (!target)
-        {
-            GameObject go = new GameObject("Cam Target");
-            go.transform.position = transform.position + (transform.forward * distance);
-            target = go.transform;
-        }
-
-        distance = Vector3.Distance(transform.position, target.position);
-        currentDistance = distance;
-        desiredDistance = distance;
-
-        //be sure to grab the current rotations as starting points.
-        position = transform.position;
-        rotation = transform.rotation;
-        currentRotation = transform.rotation;
-        desiredRotation = transform.rotation;
-
-        xDeg = Vector3.Angle(Vector3.right, transform.right);
-        yDeg = Vector3.Angle(Vector3.up, transform.up);
-    }
 
     void LateUpdate()
     {
@@ -71,49 +37,33 @@ public class ObjectInspector : MonoBehaviour
         if (Input.touchCount == 2)
         {
             Touch touchZero = Input.GetTouch(0);
-
             Touch touchOne = Input.GetTouch(1);
 
-
-
             Vector2 touchZeroPreviousPosition = touchZero.position - touchZero.deltaPosition;
-
             Vector2 touchOnePreviousPosition = touchOne.position - touchOne.deltaPosition;
 
-
-
             float prevTouchDeltaMag = (touchZeroPreviousPosition - touchOnePreviousPosition).magnitude;
-
             float TouchDeltaMag = (touchZero.position - touchOne.position).magnitude;
-
-
 
             float deltaMagDiff = prevTouchDeltaMag - TouchDeltaMag;
 
-            desiredDistance += deltaMagDiff * Time.deltaTime * zoomRate * 0.0025f * Mathf.Abs(desiredDistance);
-
-            infoText.SetActive(false); // Disable information text
+            desiredDistance += deltaMagDiff * Time.deltaTime * fineTuning * zoomRate * Mathf.Abs(desiredDistance); //0.0025f allows for finetuning of zoomRate
         }
+
         // If one or two fingers on the screen and they are moving - ORBIT!
         if (Input.touchCount == 1 || (Input.touchCount == 2 && Input.GetTouch(0).phase == TouchPhase.Moved))
         {
             Vector2 touchposition = Input.GetTouch(0).deltaPosition; //Gets position of touch movement
-            xDeg += touchposition.x * xSpeed * 0.002f; //x rotation
-            yDeg -= touchposition.y * ySpeed * 0.002f; //y rotation
+            xDeg += touchposition.x * panSpeed * fineTuning; //x rotation xSpeed * 0.002f
+            yDeg -= touchposition.y * panSpeed * fineTuning; //y rotation ySpeed * 0.002f
             yDeg = ClampAngle(yDeg, yMinLimit, yMaxLimit); //Limits the y rotation
-            
-            infoText.SetActive(false); // Disable information text
         }
-
         desiredRotation = Quaternion.Euler(yDeg, xDeg, 0);
         currentRotation = transform.rotation;
         rotation = Quaternion.Lerp(currentRotation, desiredRotation, Time.deltaTime * zoomDampening);
-        transform.rotation = rotation;
-        
+        transform.rotation = rotation;        
 
         ////////Orbit Position
-
-
         desiredDistance = Mathf.Clamp(desiredDistance, minDistance, maxDistance);
         currentDistance = Mathf.Lerp(currentDistance, desiredDistance, Time.deltaTime * zoomDampening);
 
