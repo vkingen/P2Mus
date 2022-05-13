76d75a67297ec6d1ad5855d13e12f9dbe5eef21a
Marius-Arnbjerg <marius.arnbjerg@gmail.com>
2022-05-13 09:28:51 +0200

    Nav script

--- P2NatMus/Assets/ObjectTouchInformation.cs
+++ P2NatMus/Assets/ObjectTouchInformation.cs
@@ -5,27 +5,33 @@ using TMPro;
 
 public class ObjectTouchInformation : MonoBehaviour
 {
-    public GameObject textBoxToToggle;
+    ScanningManager scanningManager;
+
+    public GameObject[] textBoxToToggle;
 
     private bool _isActive = false;
 
 
-    private void Awake()
+    private void Start()
     {
-        textBoxToToggle.SetActive(false);
+        scanningManager = FindObjectOfType<ScanningManager>();
+
+        foreach (GameObject item in textBoxToToggle)
+        {
+            item.SetActive(false);
+        }
     }
     public void ToggleInfoBox()
     {
         if(!_isActive)
         {
-            textBoxToToggle.SetActive(true);
+            textBoxToToggle[scanningManager.currentScannedObject].SetActive(true);
             _isActive = true;
         }
         else
         {
-            textBoxToToggle.SetActive(false);
+            textBoxToToggle[scanningManager.currentScannedObject].SetActive(false);
             _isActive = false;
-        }
-            
+        }            
     }
 }
